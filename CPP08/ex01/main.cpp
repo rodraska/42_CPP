ef912c7cec1dfd7d17aaa718057d678ef7f4edff
Rodrigo Reis De Carvalho <rreis-de@c2r10s3.42lisboa.com>
2023-12-08 15:00:36 +0000

    ye

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -26,6 +26,23 @@ int main(void)
         std::cout << "------------------------------------" << std::endl;
         try
         {
+            Span sp = Span(10000);
+
+            for (int i = 0; i < 10000; i ++)
+                sp.addNumber(i);
+
+            std::cout << sp.shortestSpan() << std::endl;
+            std::cout << sp.longestSpan() << std::endl;
+        }
+        catch (std::exception &exc)
+        {
+            std::cerr << exc.what() << std::endl;
+        }
+    }
+    {
+        std::cout << "------------------------------------" << std::endl;
+        try
+        {
             Span sp = Span(5);
 
             sp.addNumber(6);
