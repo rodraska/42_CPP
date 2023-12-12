3b9bad9a01f8bf0ae1b5929631d6d16b79411f3f
Rodrigo <rreiscarvalho1999@gmail.com>
2023-12-12 11:53:56 +0000

    le go

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -1,35 +1,113 @@
 #include "BitcoinExchange.hpp"
 
+int ft_btc(char* str)
+{
+    std::ifstream input_file;
+    std::ifstream data_file;
+    std::string input_line;
+    std::vector<std::string> input_tokens;
+    std::vector<std::string> data_tokens;
+
+    if (!openFile(input_file, str))
+        throw (MyCustomException("Cannot Open File"));
+    while (std::getline(input_file, input_line))
+    {
+        input_tokens = ft_split(input_line, " | ");
+        if (!checkConditions(input_tokens))
+            continue ;
+        if (!openFile(data_file, "data.csv"))
+            throw (MyCustomException("Cannot Open File"));
+        data_tokens = getData(input_tokens[0]);
+        std::cout << input_tokens[0] << " => " << input_tokens[1] << " = " << ft_stoi(input_tokens[1]) *  ft_stoi(data_tokens[1]) << std::endl;
+    }
+    input_file.close();
+    return (0);
+}
+
+std::vector<std::string> getData(std::string input_date)
+{
+    std::ifstream data_file;
+    std::string data_line;
+    std::vector<std::string> lines;
+    
+    if (!openFile(data_file, "data.csv"))
+        throw (MyCustomException("Cannot Open File"));
+    
+    while (std::getline(data_file, data_line))
+        lines.push_back(data_line);
+    std::vector<std::string>::reverse_iterator it = lines.rbegin();
+    std::vector<std::string> input_ymd = ft_split(input_date, "-");
+    while (it++ != lines.rend())
+    {
+        std::vector<std::string> data_ymd = ft_split(ft_split(*it, ",")[0], "-");
+        if (ft_stoi(data_ymd[0]) > ft_stoi(input_ymd[0]))
+            continue ;
+        if (ft_stoi(data_ymd[1]) > ft_stoi(input_ymd[1]))
+            continue ;
+        if (ft_stoi(data_ymd[2]) > ft_stoi(input_ymd[2]))
+            continue ;
+        break ;
+    }
+    data_file.close();
+    return (ft_split(*it, ","));
+}
+
+int openFile(std::ifstream& file_stream, std::string file_name)
+{
+    file_stream.open(file_name.c_str());
+    if (!file_stream.is_open())
+        return (0);
+    return (1);
+}
+
+int checkConditions(std::vector<std::string> tokens)
+{
+    double d;
+    if (tokens[0] == "date")
+        return (0);
+    if (!checkDate(tokens[0]))
+    {
+        std::cout << "Error: bad input => " << tokens[0] << std::endl;
+        return (0);
+    }
+    d = ft_stoi(tokens[1]);
+    if (d < 0)
+    {
+        std::cout << "Error: not a positive number." << std::endl;
+        return (0);
+    }
+    else if (d > 2147483647.0)
+    {
+        std::cout << "Error: too large a number." << std::endl;
+        return (0);
+    }
+    return (1);
+}
+
 int checkDate(std::string src)
 {
     std::vector<std::string> tokens = ft_split(src, "-");
-    int year;
-    int month;
-    int day;
+    int year, month, day;
     year = (int)ft_stoi(tokens[0]);
     month = (int)ft_stoi(tokens[1]);
     day = (int)ft_stoi(tokens[2]);
-    if (year < 0 || month < 0 || day < 0)
-        return (0);
-    if (month > 12)
+    if (year < 2009 || month < 0 || day < 0 || month > 12 || day > 31)
         return (0);
     if ((month == 4 || month == 6 || month == 9 || month == 11) && day > 30)
         return (0);
     if (month == 2 && day > 29)
         return (0);
-    if (day > 31)
-        return (0);
     return (1);
 }
 
-float   ft_stoi(std::string src)
+double   ft_stoi(std::string src)
 {
-    float num = 0;
+    double num = 0;
     size_t len = src.length();
     size_t i = -1;
     int f = 0;
-    float sign = 1.0;
-    float div = 0.1;
+    double sign = 1.0;
+    double div = 0.1;
     
     while (++i < len)
     {
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -5,9 +5,32 @@
 # include <fstream>
 # include <string>
 # include <vector>
+# include <exception>
+# include <cstring>
 
+
+int ft_btc(char* str);
 std::vector<std::string> ft_split(std::string src, std::string del);
-float   ft_stoi(std::string src);
+double   ft_stoi(std::string src);
 int checkDate(std::string src);
+int openFile(std::ifstream& file_stream, std::string file_name);
+int checkConditions(std::vector<std::string> tokens);
+std::vector<std::string> getData(std::string input_date);
+
+class MyCustomException : public std::exception
+{
+    private:
+        char _message[256];
+    public:
+        MyCustomException(const char* message) 
+        {
+            std::strncpy(_message, message, strlen(message));
+            _message[strlen(message)] = '\0';
+        }
+        virtual const char* what() const throw()
+        {
+            return (_message);
+        }
+};
 
 #endif
